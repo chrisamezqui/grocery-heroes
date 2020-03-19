e6f2bfadb83206eef1605f9d903411b77cbf0cd4
Chris Amezquita <christopher.amezquita@gmail.com>
2020-03-18 21:24:35 -0700

    tes t

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify#, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
