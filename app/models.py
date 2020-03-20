e69b32b321e418f533db30bf5e07ab861be3c764
Chris Amezquita <christopher.amezquita@gmail.com>
2020-03-20 14:17:00 -0700

    backend drafted

--- app/models.py
+++ app/models.py
@@ -1,25 +1,7 @@
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash
-
 from .extensions import db
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    volunteer = db.Column(db.Boolean, nullable=False)
-    recipient = db.Column(db.Boolean, nullable=False)
-
-    # @property
-    # def unhashed_password(self):
-    #     raise AttributeError('Cannot view unhashed password!')
-    #
-    # @unhashed_password.setter
-    # def unhashed_password(self, unhashed_password):
-    #     self.password = generate_password_hash(unhashed_password)
-
-class Recipient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    phone = db.Column(db.String(50), nullable=False)
     longitude = db.Column(db.Integer)
     latitude = db.Column(db.Integer)
--- app/routes/main.py
+++ app/routes/main.py
@@ -1,129 +1,42 @@
-from flask import Flask, request, jsonify, render_template
-# app = Flask(__name__)
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from app.extensions import db
+from app.models import User
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        if 'ask' in request.form:
-            return 'Old Person Page'
-        elif 'give' in request.form:
-            return 'Volunteer Page'
-    return render_template('public/index.html')
-#
-from flask import Blueprint, render_template, request, redirect, url_for
-# from flask_login import current_user, login_required
-#
-# from flask_qa.extensions import db
-# from flask_qa.models import Question, User
-#
 main = Blueprint('main', __name__)
-#
-# @main.route('/')
-# def index():
-#     questions = Question.query.filter(Question.answer != None).all()
-#
-#     context = {
-#         'questions' : questions
-#     }
-#
-#     return render_template('home.html', **context)
-#
-# @main.route('/ask', methods=['GET', 'POST'])
-# @login_required
-# def ask():
-#     if request.method == 'POST':
-#         question = request.form['question']
-#         expert = request.form['expert']
-#
-#         question = Question(
-#             question=question,
-#             expert_id=expert,
-#             asked_by_id=current_user.id
-#         )
-#
-#         db.session.add(question)
-#         db.session.commit()
-#
-#         return redirect(url_for('main.index'))
-#
-#     experts = User.query.filter_by(expert=True).all()
-#
-#     context = {
-#         'experts' : experts
-#     }
-#
-#     return render_template('ask.html', **context)
-#
-# @main.route('/answer/<int:question_id>', methods=['GET', 'POST'])
-# @login_required
-# def answer(question_id):
-#     if not current_user.expert:
-#         return redirect(url_for('main.index'))
-#
-#     question = Question.query.get_or_404(question_id)
-#
-#     if request.method == 'POST':
-#         question.answer = request.form['answer']
-#         db.session.commit()
-#
-#         return redirect(url_for('main.unanswered'))
-#
-#     context = {
-#         'question' : question
-#     }
-#
-#     return render_template('answer.html', **context)
-#
-# @main.route('/question/<int:question_id>')
-# def question(question_id):
-#     question = Question.query.get_or_404(question_id)
-#
-#     context = {
-#         'question' : question
-#     }
-#
-#     return render_template('question.html', **context)
-#
-# @main.route('/unanswered')
-# @login_required
-# def unanswered():
-#     if not current_user.expert:
-#         return redirect(url_for('main.index'))
-#
-#     unanswered_questions = Question.query\
-#         .filter_by(expert_id=current_user.id)\
-#         .filter(Question.answer == None)\
-#         .all()
-#
-#     context = {
-#         'unanswered_questions' : unanswered_questions
-#     }
-#
-#     return render_template('unanswered.html', **context)
-#
-# @main.route('/users')
-# @login_required
-# def users():
-#     if not current_user.admin:
-#         return redirect(url_for('main.index'))
-#
-#     users = User.query.filter_by(admin=False).all()
-#
-#     context = {
-#         'users' : users
-#     }
-#
-#     return render_template('users.html', **context)
-#
-# @main.route('/promote/<int:user_id>')
-# @login_required
-# def promote(user_id):
-#     if not current_user.admin:
-#         return redirect(url_for('main.index'))
-#
-#     user = User.query.get_or_404(user_id)
-#
-#     user.expert = True
-#     db.session.commit()
-#
-#     return redirect(url_for('main.users'))
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('home.html')
+
+@main.route('/helper')
+def helper():
+    return render_template('helper.html')
+
+@main.route('/helpee', methods=['GET', 'POST'])
+def helpee():
+    if request.method == 'POST':
+        phone = request.form['phone']
+        longitude = request.form['longitude']
+        latitude = request.form['latitude']
+
+        new_helpee = User(
+            phone=phone,
+            longitude=longitude,
+            latitude=latitude
+        )
+
+        db.session.add(new_helpee)
+        db.session.commit()
+
+    return render_template('helpee.html')
+
+##REST API FOR ADMIN##
+@main.route('/admin/<string:phone>', methods=['GET', 'DELETE'])
+def mod_db(phone):
+    user = User.query.filter_by(phone=phone).first()
+    if user is None:
+        return 'No such number in database'
+    if request.method == 'DELETE':
+        db.session.delete(user)
+        db.session.commit()
+    return jsonify(id=user.id, phone=user.phone, longitude=user.longitude, latitude=user.latitude)
