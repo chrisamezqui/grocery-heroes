7038889eaa398129c98ee8fdaf17148f71ad765a
Chris Amezquita <christopher.amezquita@gmail.com>
2020-03-21 14:05:51 -0700

    location use

---
+++ app/scripts/helpee.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var longitude = position.coords.latitude;
+        var latitude = position.coords.longitude;
+    }, function() {
+      handleLocationError(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false);
+  }
+  function handleLocationError(browserHasGeolocation) {
+    alert(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+  }
+}
+
+
+
+// }
+
+// function postInformation() {
+//   var formEl = document.forms.infoForm;
+//   FormData(formEl).append(longitude=longitude);
+//   FormData(formEl).append(latitude=latitude);
+//
+// }
