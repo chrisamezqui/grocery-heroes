c79b292d053cbaf79b5ad82d2b0774bb4c9c797f
Chris Amezquita <christopher.amezquita@gmail.com>
2020-03-24 17:35:27 -0700

    add info window on map point click

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from .extensions import db
 
+#latitude and longitude should function as primary keys.. maybe adjust this later. 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     phone = db.Column(db.String(50), nullable=False)
